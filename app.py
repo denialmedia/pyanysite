f7ccf8c0eeef48a4e755048d5d4ff028e4467b9a
Jason Kingery <denialmedia@gmail.com>
2023-09-05 13:00:37 -0500

    Fixed CSS seperated template from home about

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return send_from_directory('templates', 'index.html')
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
