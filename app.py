780dc43184aa3708afd30aab8285de52ad889c2b
Jason Kingery <denialmedia@gmail.com>
2023-09-05 12:04:03 -0500

    added index.html style.css updated flask

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, send_from_directory
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
-	return 'Welcome To Denial Media'
-
-# main driver function
-if __name__ == '__main__':
-
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
-
-
+    return send_from_directory('templates', 'index.html')
